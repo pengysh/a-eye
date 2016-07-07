141e1e31b0941a8f87edecc2d048c08c13945d01
ascrutae <ascrutae@gmail.com>
2016-07-07 11:25:13 +0800

    提交代码编译失败问题

--- Collector/protocol/src/main/java/com/a/eye/collector/protocol/exception/SpanTypeCannotConvertException.java
+++ Collector/protocol/src/main/java/com/a/eye/collector/protocol/exception/SpanTypeCannotConvertException.java
@@ -1,7 +1,7 @@
 package com.a.eye.collector.protocol.exception;
 
 public class SpanTypeCannotConvertException extends RuntimeException {
-    public SpanTypeCannotConvertException(String spanTypeValue) {
+    public SpanTypeCannotConvertException(int spanTypeValue) {
         super("Can not convert SpanTypeValue[" + spanTypeValue + "]");
     }
 }
